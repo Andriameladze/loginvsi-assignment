5b6e1299646d9479dfcd4ddb47fa4bb36f3f9b4f
Andria Meladze <andriameladze1@gmail.com>
2025-09-19 17:07:18 +0400

    add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NbLayoutModule, NbSidebarModule } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NbLayoutModule, NbSidebarModule],
+  imports: [RouterOutlet, RouterLink, NbLayoutModule, NbSidebarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'loginvsi-assignment2';
+  title = 'Login VSI - Task Manager';
 }
